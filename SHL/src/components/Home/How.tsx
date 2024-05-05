c9a18d2e25455e675fc8826c8ab546f3969fbc4f
Khanh Nguyen <113139168+kgn26@users.noreply.github.com>
2024-05-05 14:36:54 -0400

    finish all UI design, added animation when scroll into view

--- SHL/src/components/Home/How.tsx
+++ SHL/src/components/Home/How.tsx
@@ -1,26 +1,49 @@
 import "../../styles/How.css";
 import "animate.css";
+import { useState, useEffect } from "react";
+
 
 function How() {
+
+    const [scrollTitle, setScrollTitle] = useState(false);
+    const [scrollElt, setScrollElt] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollTitle(window.scrollY >= 1.2 * window.innerHeight);
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollElt(window.scrollY >= 1.5 * window.innerHeight);
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
     return (
         <div className="how-container">
-            <h1 className="title">Here's <span className="how-text-gradient">&nbsp;How</span></h1>
+            <h1 className={scrollTitle ? "title animate__animated animate__fadeInUp" : "title hide"}>Here's <span className="how-text-gradient">&nbsp;How</span></h1>
             <div className="card-container">
-                <div className="how-card tilt-left card-top animate__animated animate__fadeInUp animate__delay-2s">
+                <div className={scrollElt ? "how-card tilt-left card-top animate__animated animate__fadeInUp animate__delay-2s" : "how-card tilt-left card-top hide"}>
                     <h2 className="sub-title how-text-gradient">Connect:</h2>
-                    <p className="card-text">We <span className="how-text-purple">cultivate </span> a <br /> <span className="how-text-blue">vibrant network </span> of <span className="how-text-purple">innovators</span>,<br /> <span className="how-text-blue">mentors</span>, and <br /> <span className="how-text-purple">industry leaders</span>, creating <br /> <span className="how-text-blue">opportunities </span> for <br /> <span className="how-content-gradient">meaningful</span> <br /> <span className="how-content-gradient">exchange and</span> <br /> <span className="how-content-gradient">growth</span>.</p>
+                    <p className="card-text">We <span className="how-text-purple">cultivate </span> a  <span className="how-text-blue">vibrant network </span> of <span className="how-text-purple">innovators</span>, <span className="how-text-blue">mentors</span>, and  <span className="how-text-purple">industry leaders</span>, creating  <span className="how-text-blue">opportunities </span> for  <span className="how-content-gradient">meaningful</span>  <span className="how-content-gradient">exchange and</span>  <span className="how-content-gradient">growth</span>.</p>
                 </div>
-                <div className="how-card tilt-right card-mid animate__animated animate__fadeInUp animate__delay-1s">
+                <div className={scrollElt ? "how-card tilt-right card-mid animate__animated animate__fadeInUp animate__delay-1s" : "how-card tilt-right card-top hide"}>
                     <h2 className="sub-title how-text-gradient">Event:</h2>
-                    <p className="card-text">We strive to Advance Education through Tailored Events that Foster Curiosity, <br /> Critical Thinking, and <br /> Skill Development Beyond Traditional Academics.</p>
+                    <p className="card-text">We strive to <span className="how-text-blue">Advance </span> <span className="how-text-purple">Education </span> through <span className="how-text-blue">Tailored </span> <span className="how-text-purple">Events </span> that <span className="how-text-blue">Foster </span> <span className="how-text-purple">Curiosity</span>,  <span className="how-text-blue">Critical </span> <span className="how-text-purple">Thinking</span>, and  <span className="how-text-blue">Skill </span> <span className="how-text-purple">Development </span> <span className="how-content-gradient">Beyond </span> <span className="how-text-blue">Traditional </span> <span className="how-text-purple"> Academics</span>.</p>
                 </div>
-                <div className="how-card tilt-left card-bot animate__animated animate__fadeInUp">
+                <div className={scrollElt ? "how-card tilt-left card-bot animate__animated animate__fadeInUp" : "how-card tilt-left card-top hide"}>
                     <h2 className="sub-title how-text-gradient">Support:</h2>
-                    <p className="card-text">We Empower Event Organizers by providing Personalized Support, fostering Invaluable Experiences, incentivizing <br /> Sustainable and Futuristic Development, and Paving the Way to a Better Future.</p>
+                    <p className="card-text">We <span className="how-content-gradient"> Empower </span> <span className="how-text-blue">Event </span> <span className="how-text-purple">Organizers </span> by providing <span className="how-text-blue">Personalized </span> <span className="how-text-purple">Support</span>, fostering <span className="how-text-blue">Invaluable </span> <span className="how-text-purple">Experiences</span>, incentivizing <span className="how-text-blue">Sustainable </span> and <span className="how-text-purple">Futuristic Development</span>, and <span className="how-text-blue">Paving </span> the <span className="how-text-purple">Way </span> to a <span className="how-content-gradient">Better Future</span>.</p>
                 </div>
             </div>
         </div>
     );
 }
+
 
 export default How;
--- SHL/src/components/Home/Quote.tsx
+++ SHL/src/components/Home/Quote.tsx
@@ -1,43 +1,66 @@
 import "../../styles/Quote.css";
 import "animate.css";
+import { useState, useEffect } from "react";
 
 function Quote() {
+
+    const [scrollTitle, setScrollTitle] = useState(false);
+    const [scrollElt, setScrollElt] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollTitle(window.scrollY >= 0.2 * window.innerHeight);
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollElt(window.scrollY >= 0.2 * window.innerHeight);
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
     return (
-        <div className="quote-container">
-            {/* <img className="animate__animated animate__fadeInUp" id="quote-top" src="/quotes.png" alt="" /> */}
-            {/* <img className="animate__animated animate__fadeInUp" id="quote-bot" src="/quotes.png" alt="" /> */}
-            <p className="animate__animated animate__fadeInUp">
-                <span className="text-purple">Star Hack </span>
-                is
-                <span className="text-blue"> more </span>
-                than
-                <span className="text-purple"> just a </span><br />
-                <span className="text-blue"> platform </span>
-                – it's a
-                <span className="text-purple"> community</span>
-                . <br /> We carefully
-                <span className="text-blue"> curate </span>
-                <span className="text-purple"> hackathons </span>
-                that <br />
-                <span className="text-blue"> align </span>
-                with our sponsors'
-                <span className="text-purple"> values</span>
-                , <br /> ensuring
-                <span className="text-blue"> meaningful </span>
-                <span className="text-purple"> partnerships </span> <br />
-                that drive
-                <span className="text-blue"> positive </span>
-                <span className="text-purple"> change </span>
-                within the <br />
-                <span className="text-blue"> tech industry </span>
-                and
-                <span className="text-gradient"> beyond</span>.
-            </p>
-            <p className="author animate__animated animate__fadeInUp animate__delay-1s">
-                -Shaan Yu <br />
-                Co-Founder @ Star Hack
-            </p>
-        </div>
+        <>
+            <div className="quote-container">
+                <img className={scrollTitle ? "animate__animated animate__fadeInUp" : "hide"} id="quote-bot" src="/quotes.png" alt="" />
+                <img className={scrollTitle ? "animate__animated animate__fadeInUp" : "hide"} id="quote-top" src="/quotes.png" alt="" />
+                <p className={scrollTitle ? "quote-content animate__animated animate__fadeInUp" : "hide"}>
+                    <span className="text-purple">Star Hack </span>
+                    is
+                    <span className="text-blue"> more </span>
+                    than
+                    <span className="text-purple"> just a </span>
+                    <span className="text-blue"> platform </span>
+                    – it's a
+                    <span className="text-purple"> community</span>
+                    .  We carefully
+                    <span className="text-blue"> curate </span>
+                    <span className="text-purple"> hackathons </span>
+                    that
+                    <span className="text-blue"> align </span>
+                    with our sponsors'
+                    <span className="text-purple"> values</span>
+                    ,  ensuring
+                    <span className="text-blue"> meaningful </span>
+                    <span className="text-purple"> partnerships </span>
+                    that drive
+                    <span className="text-blue"> positive </span>
+                    <span className="text-purple"> change </span>
+                    within the
+                    <span className="text-blue"> tech industry </span>
+                    and
+                    <span className="text-gradient"> beyond</span>.
+                </p>
+                <p className={scrollElt ? "author animate__animated animate__fadeInUp animate__delay-1s" : "author hide"}>
+                    -Shaan Yu
+                    Co-Founder @ Star Hack
+                </p>
+            </div >
+        </>
     );
 }
 
