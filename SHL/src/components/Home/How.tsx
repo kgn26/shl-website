12be4f64d560e4267d05f23e4457746a148ade17
Khanh Nguyen <113139168+kgn26@users.noreply.github.com>
2024-06-01 17:51:57 +0700

    added scroll-snap and fix alignment issues

--- SHL/src/components/Home/How.tsx
+++ SHL/src/components/Home/How.tsx
@@ -28,11 +28,11 @@ function How() {
         <div className="how-container">
             <h1 className={scrollTitle ? "title animate__animated animate__fadeInUp" : "title hide"}>Here's <span className="how-text-gradient">&nbsp;How</span></h1>
             <div className="card-container">
-                <div className={scrollElt ? "how-card tilt-left card-top animate__animated animate__fadeInUp animate__delay-2s" : "how-card tilt-left card-top hide"}>
+                <div className={scrollElt ? "how-card tilt-left card-top animate__animated animate__fadeInUp animate__delay__second" : "how-card tilt-left card-top hide"}>
                     <h2 className="sub-title how-text-gradient">Connect:</h2>
                     <p className="card-text">We <span className="how-text-purple">cultivate </span> a  <span className="how-text-blue">vibrant network </span> of <span className="how-text-purple">innovators</span>, <span className="how-text-blue">mentors</span>, and  <span className="how-text-purple">industry leaders</span>, creating  <span className="how-text-blue">opportunities </span> for  <span className="how-content-gradient">meaningful</span>  <span className="how-content-gradient">exchange and</span>  <span className="how-content-gradient">growth</span>.</p>
                 </div>
-                <div className={scrollElt ? "how-card tilt-right card-mid animate__animated animate__fadeInUp animate__delay-1s" : "how-card tilt-right card-top hide"}>
+                <div className={scrollElt ? "how-card tilt-right card-mid animate__animated animate__fadeInUp animate__delay__first" : "how-card tilt-right card-top hide"}>
                     <h2 className="sub-title how-text-gradient">Event:</h2>
                     <p className="card-text">We strive to <span className="how-text-blue">Advance </span> <span className="how-text-purple">Education </span> through <span className="how-text-blue">Tailored </span> <span className="how-text-purple">Events </span> that <span className="how-text-blue">Foster </span> <span className="how-text-purple">Curiosity</span>,  <span className="how-text-blue">Critical </span> <span className="how-text-purple">Thinking</span>, and  <span className="how-text-blue">Skill </span> <span className="how-text-purple">Development </span> <span className="how-content-gradient">Beyond </span> <span className="how-text-blue">Traditional </span> <span className="how-text-purple"> Academics</span>.</p>
                 </div>
--- SHL/src/pages/Home.tsx
+++ SHL/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Landing from "../components/Home/Landing";
 import Quote from "../components/Home/Quote"
 import How from "../components/Home/How";
+import "../styles/Home.css"
 
 function Home() {
   return (
